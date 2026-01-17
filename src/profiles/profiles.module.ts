0ea75af1c9c9f4beb38f30c31ebde0be74d48f48
rodrigozucchini <rodrigozucchini1997@gmail.com>
2026-01-17 01:44:26 +0000

    add the entities and add in the file txt all the users files estructure

---
+++ src/profiles/profiles.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { AdminProfileModule } from './admin-profile/admin-profile.module';
+import { ClientProfileModule } from './client-profile/client-profile.module';
+
+@Module({
+  imports: [AdminProfileModule, ClientProfileModule]
+})
+export class ProfilesModule {}
